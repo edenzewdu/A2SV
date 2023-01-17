9d96a9703a01c0d0633104992bf97b9cd20f8632
edenzewdu <88831934+edenzewdu@users.noreply.github.com>
2023-01-17 21:09:24 +0300

    Add files via upload

---
+++ Find Original Array From Doubled Array - LeetCode.java
@@ -0,0 +1,24 @@
+﻿class Solution {
+    public int[] findOriginalArray(int[] changed) {
+        int[] n;
+        int empty [] = {};
+        n = new int[100];
+        n = Arrays.copyOf(n,changed.length/2);
+        int k=changed.length/2;
+        if(changed.length%2==0){
+            for(int i=0;i<changed.length/2;i++){
+                if(changed[i]*2==(changed[k])){
+                    n[i]=changed[i];
+                    k++;
+                }
+                
+                else
+                return empty;
+            }
+        }
+        else{
+            return empty;
+        }
+         return n;
+    }
+}
