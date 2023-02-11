1258aa2d3e3bca565cf7e6ee3ec563f442f60564
edenzewdu <88831934+edenzewdu@users.noreply.github.com>
2023-02-11 10:49:11 +0300

    Add files via upload

---
+++ Maximum Number of Coins You Can Get - LeetCode.java
@@ -0,0 +1,15 @@
+﻿
+class Solution {
+    public static int  maxCoins(int[] piles) {
+          Arrays.sort(piles);
+        int res = 0, n = piles.length;
+        for (int i = n / 3; i < n; i += 2)
+            res += piles[i];
+        return res;
+    }
+    public static void main(String[] args) {
+        int [] arr = {9,8,7,6,5,1,2,3,4}; 
+        int ans=  maxCoins(arr);
+        System.out.println(ans);
+    }
+}
