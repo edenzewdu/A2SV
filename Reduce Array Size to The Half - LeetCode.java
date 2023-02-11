091e64a1b0f76d65186f7213f3debddce3fe1f49
edenzewdu <88831934+edenzewdu@users.noreply.github.com>
2023-02-11 11:06:38 +0300

    Add files via upload

---
+++ Reduce Array Size to The Half - LeetCode.java
@@ -0,0 +1,24 @@
+﻿class Solution {
+     public int minSetSize(int[] arr) {
+        int max = Integer.MIN_VALUE;
+         for (int num : arr) {
+            max = Math.max(max, num);
+        }
+
+        int[] count = new int[max + 1];
+       for (int num : arr) {
+          count[num]++;
+         }
+         Arrays.sort(count);
+         int half = arr.length / 2;
+         int res = 0;
+        for (int i = count.length - 1; i >= 0; i--) {
+            half -= count[i];
+             res++;
+            if (half <= 0) {
+                 return res;
+            }
+         }
+         return arr.length;
+     }
+ }
