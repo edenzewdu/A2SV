813aea974dbee2e28ffef2e59b4a010e793179ee
edenzewdu <88831934+edenzewdu@users.noreply.github.com>
2023-05-16 17:43:27 +0300

    Update and rename Problem - 50A - Codeforces to Problem - 50A - Codeforces.py

---
+++ Problem - 50A - Codeforces.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def fizzBuzz(n, m):
+        """
+        :type n: int
+        :type m: int
+        """
+        n = input()
+        m = input()
+        a = n*m
+        print(a/2)
+        
