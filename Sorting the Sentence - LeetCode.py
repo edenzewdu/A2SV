3b26ec69f9c795d30a9e401dcea8bc1cd3b0165f
edenzewdu <88831934+edenzewdu@users.noreply.github.com>
2023-05-17 04:33:14 +0300

    Create Sorting the Sentence - LeetCode.py

---
+++ Sorting the Sentence - LeetCode.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortSentence(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s = s.split()
+        ans = [None]*len(s)
+        for i in range(len(s)):
+            ans[int(s[i][-1])-1] = s[i][:-1]
+        return ' '.join(r)
